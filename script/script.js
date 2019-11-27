93beface0dee32ca928fbf8e9b30a1932a43e7cb
Angelo <angelo.vanrooij@student.fontys.nl>
2019-11-27 15:15:08 +0100

    Karakter beweegt links en rechts
    Hij kan links en rechts bewegen en geprobeerd om vloeiend te laten lopen maar geen succes.
    vooral gekloot dus probeer later nog eens.
    Nu eerst bal laten met zwaartekracht

--- script/script.js
+++ script/script.js
@@ -1,22 +1,65 @@
 let catcher = document.getElementById("catcher")
 let moveby = 10;
-var LeftPosition = 100
+var position = 0;
 
 
 window.onkeydown = function (e) {
-    MovePlatform(e);
+    MovePlatform(e)
 }
+
+/*er moet een parameter worden toegevoegd (key), vervolgens is het argument de button*/
 
 function MovePlatform(key) {
 
     switch (key.code) {
         case "ArrowLeft":
-            catcher.style.left = catcher.style.left - moveby + "px";
-            //per 10 (moveby) naar links (vloeiend)
+            position = position - moveby;
+            catcher.style.left = position + "px";
+            console.log(moveby);
             break;
         case "ArrowRight":
-                catcher.style.left = catcher.style.left + moveby + "px";
+            position = position + moveby;
+            catcher.style.left = position + "px";
             //per 10 (moveby) naar links (vloeiend)
+            console.log(moveby);
             break;
     }
 }
+
+/* ipv de afbeelding te laten verspringen per 10, doe ik hem per 1 maar dan icm een timer zodat ie vloeiender gaat
+   ipv te updaten op een click, constant locatie updaten*/
+function Timer(key) {
+    if (key.code == "ArrowLeft") {
+        position -= 1;
+    } else if (key.code == "ArrowRight") {
+        position += 1;
+    }
+
+    catcher.style.left = position + "px";
+
+    setTimeout(Timer, 10)
+
+}
+
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+/*function MovePlatform(key){
+    if (key.code == "ArrowLeft"){
+        MoveLeft();
+    }
+
+    else if (key.code == "ArrowRight"){
+        MoveRight();
+    }
+
+}
+function MoveLeft() {
+    catcher.style.left = catcher.style.left - moveby + "px";
+}
+
+function MoveRight() {
+    catcher.style.left = catcher.style.left + moveby + "px";
+}
+*/
